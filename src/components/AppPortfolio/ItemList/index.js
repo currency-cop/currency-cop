c8b6f4409afb320eb8b596230a7de2f13e763867
Nijiko Yonskai <nijikokun@gmail.com>
2018-05-16 02:45:55 -0700

    feat(portfolio): add sorting

--- src/components/AppPortfolio/ItemList/index.js
+++ src/components/AppPortfolio/ItemList/index.js
@@ -4,16 +4,30 @@ import './index.css'
 import Item from './Item'
 
 class PortfolioItemList extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.sortByName = this.sortByName.bind(this)
+    this.sortByQuantity = this.sortByQuantity.bind(this)
+    this.sortByPrice = this.sortByPrice.bind(this)
+    this.sortByTotal = this.sortByTotal.bind(this)
+
+    this.state = {
+      sortBy: 'total-desc',
+      sort: null
+    }
+  }
+
   render () {
     return (
       <table>
         <thead>
           <tr>
             <th>Icon</th>
-            <th>Item Name</th>
-            <th>Quantity</th>
-            <th>Price</th>
-            <th>Total</th>
+            <th onClick={this.sortByName}>Item Name {this.isSortedBy('name')}</th>
+            <th onClick={this.sortByQuantity}>Quantity {this.isSortedBy('quantity')}</th>
+            <th onClick={this.sortByPrice}>Price {this.isSortedBy('price')}</th>
+            <th onClick={this.sortByTotal}>Total {this.isSortedBy('total')}</th>
           </tr>
         </thead>
         <tbody>
@@ -23,8 +37,73 @@ class PortfolioItemList extends React.Component {
     )
   }
 
+  isSortedBy (name) {
+    if (this.state.sortBy && this.state.sortBy.indexOf(name) > -1) {
+      if (this.state.sortBy.split('-')[1] === 'asc') {
+        return (<i className="material-icons">&#xe5c7;</i>)
+      }
+  
+      return (<i className="material-icons">&#xE5C5;</i>)
+    }
+
+    return ``
+  }
+
+  sortBy (name, asc, desc) {
+    let sortBy = this.state.sortBy === `${name}-asc`
+      ? `${name}-desc`
+      : `${name}-asc`
+
+    let sort = this.state.sortBy === `${name}-asc`
+      ? desc
+      : asc
+
+    this.setState({
+      sortBy,
+      sort
+    })
+  }
+
+  sortByQuantity () {
+    this.sortBy('quantity', (a, b) => {
+      return a.stackSize - b.stackSize
+    }, (a, b) => {
+      return b.stackSize - a.stackSize
+    })
+  }
+
+  sortByPrice () {
+    this.sortBy('price', (a, b) => {
+      return a.item.chaosValue - b.item.chaosValue
+    }, (a, b) => {
+      return b.item.chaosValue - a.item.chaosValue
+    })
+  }
+
+  sortByTotal () {
+    this.sortBy('total', (a, b) => {
+      return a.chaosValue - b.chaosValue
+    }, (a, b) => {
+      return b.chaosValue - a.chaosValue
+    })
+  }
+
+  sortByName () {
+    this.sortBy('name', (a, b) => {
+      return a.item.fullName.localeCompare(b.item.fullName)
+    }, (a, b) => {
+      return b.item.fullName.localeCompare(a.item.fullName)
+    })
+  }
+
   renderItems () {
-    return this.props.items.map((details, index) => {
+    let items = this.props.items
+
+    if (this.state.sort) {
+      items = items.sort(this.state.sort)
+    }
+
+    return items.map((details, index) => {
       return (
         <Item 
           key={`${this.props.id}-${index}-${details.item.fullName}`}
