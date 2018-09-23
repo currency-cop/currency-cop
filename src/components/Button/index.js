df4897e235c5d2daed9e564779fa6ce216fa163a
Alaric von Teplitz <alaric2000@gmail.com>
2018-09-23 10:31:35 -0600

    Refactored the Button component to see if this style would be accepted.

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,24 +1,14 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
 import styles from './index.css'
 
-class Button extends React.Component {
-  constructor (props) {
-    super(props)
-  }
+const Button = (props) => {
+  const className = props.className ? `btn ${props.className}` : 'btn';
 
-  render () {
-    let className = `btn`
-
-    if (this.props.className) {
-      className += ` ${this.props.className}`
-    }
-
-    return (
-      <button {...this.props} onClick={this.props.onClick} className={className}>
-        {this.props.children}
-      </button>
-    )
-  }
+  return (
+    <button onClick={props.onClick} className={className}>
+      {props.children}
+    </button>
+  );
 }
 
-export default Button
+export default Button;
